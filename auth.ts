d0ecd6d3882e8029628d89b5cf62d73277894e76
azkriven <azkriven17@gmail.com>
2025-04-03 14:22:02 +0800

    ✨feat: shadcn ui authjs

---
+++ auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db";
+import { accounts, sessions, users } from "./db/schema";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    adapter: DrizzleAdapter(db, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+    }),
+    providers: [Google],
+});
